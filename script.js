92bfc70abc8475c79c742d99ffcf0db2e1a22a24
Sarah Johnson <sarah@Sarahs-MacBook-Pro.local>
2025-08-27 21:13:28 -0400

    play dem keys

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const keys = document.querySelectorAll('.key');
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
 
 keys.forEach((key) => {
   key.addEventListener('click', () => playNote(key));
@@ -19,3 +25,22 @@ function playNote(key) {
   });  
   
 }
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+    if (e.repeat) {
+    return;
+  }
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
